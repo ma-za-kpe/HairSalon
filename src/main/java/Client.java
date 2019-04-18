f9e0cd55dcf82378f57e6ce7337db8aa8114ed1e
ma-za-kpe <makpalyy@gmail.com>
2019-04-18 14:35:20 +0200

    intergrate db fuctionality to entire Client class including tests

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,3 +1,58 @@
-public class Client {
+import org.sql2o.Connection;
 
+import java.util.List;
+
+public class Client {
+    private String name;
+    private int id;
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Client find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM clients where id=:id";
+            Client client = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Client.class);
+            return client;
+        }
+    }
+
+    public static List<Client> all() {
+        String sql = "SELECT id, name FROM clients";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Client.class);
+        }
+    }
+
+    @Override
+    public boolean equals(Object otherClient){
+        if (!(otherClient instanceof Client)) {
+            return false;
+        } else {
+            Client newClient = (Client) otherClient;
+            return this.getName().equals(newClient.getName()) &&
+                    this.getId() == newClient.getId();
+        }
+    }
+
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO clients (name) VALUES (:name)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("description", this.name)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,5 +1,6 @@
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
@@ -18,6 +19,59 @@ public class ClientTest {
             String sql = "DELETE FROM clients *;";
             con.createQuery(sql).executeUpdate();
         }
+    }
+
+    //compare objects we retrieve from the database with our code in the all() method
+    @Test
+    public void equals_returnsTrueIfDescriptionsAretheSame() {
+        Client firstClient = new Client("Mama Tendo");
+        Client secondClient = new Client("Morine");
+        assertTrue(firstClient.equals(secondClient));
+    }
+
+    //save objects into our database
+    @Test
+    public void save_returnsTrueIfDescriptionsAretheSame() {
+        Client myClient = new Client("Mama Tendo");
+        myClient.save();
+        assertTrue(Client.all().get(0).equals(myClient));
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfTask_true() {
+        Client firstClient = new Client("Mama Tendo");
+        firstClient.save();
+        Client secondClient = new Client("Morine");
+        secondClient.save();
+        assertEquals(true, Client.all().get(0).equals(firstClient));
+        assertEquals(true, Client.all().get(1).equals(secondClient));
+    }
+
+    //testing the id
+    @Test
+    public void save_assignsIdToObject() {
+        Client myClient = new Client("Mama Tendo");
+        myClient.save();
+        Client savedClient = Client.all().get(0);
+        assertEquals(myClient.getId(), savedClient.getId());
+    }
+
+    //test for getid
+    @Test
+    public void getId_tasksInstantiateWithAnID() {
+        Client myClient = new Client("Mama Tendo");
+        myClient.save();
+        assertTrue(myClient.getId() > 0);
+    }
+
+    //finding specific objects
+    @Test
+    public void find_returnsTaskWithSameId_secondTask() {
+        Client firstClient = new Client("Oliver");
+        firstClient.save();
+        Client secondClient = new Client("Mary");
+        secondClient.save();
+        assertEquals(Client.find(secondClient.getId()), secondClient);
     }
 
 }
