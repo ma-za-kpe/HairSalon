8085f9bbbf1a3088a0afc12311046e95cfd0ba0f
ma-za-kpe <makpalyy@gmail.com>
2019-04-18 14:05:36 +0200

    create client and test files

---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,23 @@
+import org.junit.After;
+import org.junit.Before;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static org.junit.Assert.*;
+
+public class ClientTest {
+
+    @Before
+    public void setUp() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", "maku", "maku");
+    }
+
+    @After
+    public void tearDown() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "DELETE FROM clients *;";
+            con.createQuery(sql).executeUpdate();
+        }
+    }
+
+}
