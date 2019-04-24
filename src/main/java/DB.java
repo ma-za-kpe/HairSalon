b196e47da4c48f3d8782ea2cf67cb4f300614a11
ma-za-kpe <makpalyy@gmail.com>
2019-04-24 20:00:31 +0200

    remove the redundant 77 on the stylistTodo.vtl file

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,7 +1,7 @@
 import org.sql2o.Sql2o;
 
 public class DB {
-//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "maku", "maku");
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-50-17-246-114.compute-1.amazonaws.com:5432/d9eslj80tejpha", "yphcwhpvxgzoyk", "7f7c6c9c8440af98a796ec9a1598a901f517715a38141db1e2570f026540dafb");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "maku", "maku");
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-50-17-246-114.compute-1.amazonaws.com:5432/d9eslj80tejpha", "yphcwhpvxgzoyk", "7f7c6c9c8440af98a796ec9a1598a901f517715a38141db1e2570f026540dafb");
 
 }
